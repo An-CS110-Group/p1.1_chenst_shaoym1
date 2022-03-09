9e05fd4531d8e1c7fff3d30db9807023cc634efe
caoster <caoster@icloud.com>
2022-03-10 02:34:45 +0800

    Structs, templates for new code! I'm so so so tired.

--- src/compression.c
+++ src/compression.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "compression.h"
 #include "utils.h"
 
-/* Your code here... */
-
+Compressed **primaryCompression(const Instruction **source) {
+	Compressed **target;
+	/* 1. Check validation */
+	if (source == NULL) { return NULL; }
+	/* 2. Allocate spce for pointers */
+	target = malloc(sizeof(Compressed *) * 60);
+	return target;
+}
